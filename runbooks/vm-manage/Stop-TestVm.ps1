28bfb819eb59b91692ae0333569f7304f7c725f6
Zethuil <Zethuil@users.noreply.github.com>
2020-04-22 16:30:44 +0200

    Update Stop-TestVm.ps1

--- runbooks/vm-manage/Stop-TestVm.ps1
+++ runbooks/vm-manage/Stop-TestVm.ps1
@@ -2,7 +2,10 @@ workflow Stop-TestVm
 {
  Param(
   [string]$VM,
-  [string]$ResourceGroup
+  [string]$ResourceGroup,
+  
+  [Parameter (Mandatory = $false)]
+  [object] $WebhookData
  )
 # Ensures that you do not inherit an AzContext in your runbook
 Disable-AzContextAutosave –Scope Process
@@ -11,5 +14,13 @@ $Conn = Get-AutomationConnection -Name AzureRunAsConnection
 Connect-AzAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
 Set-AzContext -SubscriptionId "f32205a4-a094-4fd9-8b98-3122eb6b1889"
 
-Stop-AzVM -Name $VM -ResourceGroupName $ResourceGroup -Force
+if ($WebhookData) 
+{
+ Stop-AzVM -Name (ConvertFrom-Json -InputObject $WebhookData.RequestBody).VM -ResourceGroupName (ConvertFrom-Json -InputObject $WebhookData.RequestBody).ResourceGroup
+}
+else
+{
+ Stop-AzVM -Name $VM -ResourceGroupName $ResourceGroup
+}
+
 }
